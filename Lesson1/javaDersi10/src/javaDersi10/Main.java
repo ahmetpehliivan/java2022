cd8449af8c2add91be86a10112acb3b6604c848a
ahmetpehliivan <afpehlivan18@gmail.com>
2022-09-19 01:38:28 +0300

    Examples

---
+++ Lesson1/javaDersi10/src/javaDersi10/Main.java
@@ -0,0 +1,30 @@
+package javaDersi10;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade ='C';
+		
+		switch(grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		case 'B':
+			System.out.println("Çok iyi : Geçtiniz");
+			break;
+		case 'C':
+			System.out.println("İyi : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Fena değil : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Malesef kaldınız..");
+			break;
+		default:
+			System.out.println("Lütfen geçerli bir kod giriniz..");
+		}
+
+	}
+
+}
