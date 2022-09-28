7de529e5254ebc824e62c923d7a88db331485795
Ahmet Pehlivan <afpehlivan18@gmail.com>
2022-09-28 15:45:58 +0300

    Method Overloading

---
+++ Lesson2/javaDersi35/src/javaDersi35/Main.java
@@ -0,0 +1,11 @@
+package javaDersi35;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FourTransactions fourTransactions = new FourTransactions();
+		System.out.println(fourTransactions.sum(2, 3));
+		System.out.println(fourTransactions.sum(3, 45, 5));
+	}
+
+}
