0e45296ed684264949fbc7208791e95f3ae3234b
Ahmet Pehlivan <afpehlivan18@gmail.com>
2022-09-28 13:16:53 +0300

    ReCap Demo(Class)

---
+++ Lesson2/javaDersi30/src/FourOperation.java
@@ -0,0 +1,15 @@
+public class FourOperation {
+    public int sum (int number1, int number2){
+        return number1+number2;
+    }
+    public int substract (int number1, int number2){
+        return number1-number2;
+    }
+    public int multiply (int number1, int number2){
+        return number1*number2;
+    }
+    public int divide (int number1, int number2){
+        return number1/number2;
+    }
+
+}
---
+++ Lesson2/javaDersi30/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        FourOperation fourOperation = new FourOperation();
+        int result = fourOperation.divide(10,2);
+        System.out.println(result);
+
+    }
+}
